b9abeff67768210c3c357c489848e03982534e01
Adokshaj Bhandarkar <badokshaj57@gmail.com>
2023-02-07 18:55:10 +0530

    all 4 buttons functionalities working now

--- tkinter_gui.py
+++ tkinter_gui.py
@@ -108,7 +108,7 @@ def generate():
         username="4nm19is120",
         password="27102022"
         )
-        stdin, stdout, stderr = ssh.exec_command("docker exec lightningsliver sh -c 'cd home/4nm19is120/text_to_image/Text-to-Image-Using-GAN-master/text_to_image/Text-to-Image-Using-GAN-master/ && python3 generate_images_original.py --data_set=flowers --t_dim=100 --image_size=128 --data_set=flowers --z_dim=100 --n_classes=24 --caption_vector_length=4800 --batch_size=128  --checkpoints_dir=Data/training/TAC_128/checkpoints --images_per_caption=30 --data_dir=Data && cd Data && rm download.zip && zip -r download.zip images_generated_from_text/* && ls'")
+        stdin, stdout, stderr = ssh.exec_command("docker exec lightningsliver sh -c 'cd home/4nm19is120/text_to_image/Text-to-Image-Using-GAN-master/text_to_image/Text-to-Image-Using-GAN-master/ && python3 generate_images_original.py --data_set=flowers --t_dim=100 --image_size=128 --data_set=flowers --z_dim=100 --n_classes=24 --caption_vector_length=4800 --batch_size=128  --checkpoints_dir=Data/training/TAC_128/checkpoints --images_per_caption=30 --data_dir=Data && cd Data && rm download.zip && zip -r download.zip images_generated_from_text/* && rm -rf images_generated_from_text/ &&  ls'")
         print(stdout.read().decode())
 
         sftp = ssh.open_sftp()
@@ -120,13 +120,13 @@ def generate():
         shutil.unpack_archive('./download.zip')
         ################################################
 
-        # messagebox.showinfo('Success!', 'Your images have been successfully generated!\n\nCLICK \'OK\' TO SEE YOUR IMAGE.')
+        messagebox.showinfo('Success!', 'Your images have been successfully generated!\n\nCLICK \'Display Image Button\' TO SEE YOUR IMAGE.')
 
 def display():
         frame = Frame(root)
         frame.pack()
         frame.place(anchor='center', relx=0.5, rely=0.6)
-        toDisplay = ImageTk.PhotoImage(Image.open("0.jpg"))
+        toDisplay = ImageTk.PhotoImage(Image.open(".\images_generated_from_text\\0\\0.jpg"))
         label  = Label(frame, image = toDisplay)
         label.image = toDisplay
         label.pack()
