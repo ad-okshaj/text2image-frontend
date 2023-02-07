78ea8d250050498d6a2be5db31d5ffbc9236c57a
Adokshaj Bhandarkar <badokshaj57@gmail.com>
2023-02-07 14:17:04 +0530

    merging paramiko3 + gui file

---
+++ paramiko3.py
@@ -0,0 +1,21 @@
+import paramiko
+
+ssh = paramiko.SSHClient()
+ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+ssh.connect(
+    hostname="gpu.nmamit.in",
+    port=202,
+    username="4nm19is120",
+    password="27102022"
+)
+
+stdin, stdout, stderr = ssh.exec_command("docker exec lightningsliver sh -c 'cd home/4nm19is120/text_to_image/Text-to-Image-Using-GAN-master/text_to_image/Text-to-Image-Using-GAN-master/ && python3 generate_images_original.py --data_set=flowers --t_dim=100 --image_size=128 --data_set=flowers --z_dim=100 --n_classes=24 --caption_vector_length=4800 --batch_size=128  --checkpoints_dir=Data/training/TAC_128/checkpoints --images_per_caption=30 --data_dir=Data && cd Data && rm download.zip && zip -r download.zip images_generated_from_text/* && ls'")
+print(stdout.read().decode())
+
+sftp = ssh.open_sftp()
+sftp.get('/home/4nm19is120/text_to_image/Text-to-Image-Using-GAN-master/Data/download.zip', 'download.zip')
+sftp.close()
+ssh.close()
+
+import shutil
+shutil.unpack_archive('./download.zip')
--- tkinter_gui.py
+++ tkinter_gui.py
@@ -1,7 +1,6 @@
+import os
 from tkinter import *
 from tkinter import messagebox
-
-import os
 root = Tk()
 
 #Below line will open application in full screen mode
@@ -43,11 +42,32 @@ def save_text():
         messagebox.showinfo("File Saved.","Your text has been saved.\nPlease proceed with Image Generation.\nClick 'Generate Image' button now.")
 
 
-def take_input():
-        #Below input variable will contain the INPUT of user, which has to be sent to server
-	INPUT = inputtext.get("1.0", "end-1c")
-	#To check whether the INPUT is passed in separate lines or not
-	Output.insert(END, INPUT)
+def generate():
+        # #Below input variable will contain the INPUT of user, which has to be sent to server
+	# INPUT = inputtext.get("1.0", "end-1c")
+	# #To check whether the INPUT is passed in separate lines or not
+	# Output.insert(END, INPUT)
+        messagebox.showinfo("Waiting", "Waiting...")
+        ################################################
+        import paramiko
+        ssh = paramiko.SSHClient()
+        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        ssh.connect(
+        hostname="gpu.nmamit.in",
+        port=202,
+        username="4nm19is120",
+        password="27102022"
+        )
+        stdin, stdout, stderr = ssh.exec_command("docker exec lightningsliver sh -c 'cd home/4nm19is120/text_to_image/Text-to-Image-Using-GAN-master/text_to_image/Text-to-Image-Using-GAN-master/ && python3 generate_images_original.py --data_set=flowers --t_dim=100 --image_size=128 --data_set=flowers --z_dim=100 --n_classes=24 --caption_vector_length=4800 --batch_size=128  --checkpoints_dir=Data/training/TAC_128/checkpoints --images_per_caption=30 --data_dir=Data && cd Data && rm download.zip && zip -r download.zip images_generated_from_text/* && ls'")
+        print(stdout.read().decode())
+
+        sftp = ssh.open_sftp()
+        sftp.get('/home/4nm19is120/text_to_image/Text-to-Image-Using-GAN-master/Data/download.zip', 'download.zip')
+        sftp.close()
+        ssh.close()
+
+        import shutil
+        shutil.unpack_archive('./download.zip')
     
 	
 l = Label(text = "Enter the description of the image to be generated: ")
@@ -55,7 +75,7 @@ inputtext = Text(root, height = 10, width = 150, bg = "light yellow")
 Output = Text(root, height = 20, width = 150)
 open_button = Button(root, height = 2, width = 20, text ="Create / Open Text File", command = lambda:open_text())
 save = Button(root, height = 2, width = 20, text="Save File", command = lambda:save_text())
-Display = Button(root, height = 2, width = 20, text ="Generate Image", command = lambda:take_input())
+Display = Button(root, height = 2, width = 20, text ="Generate Image", command = lambda:generate())
 
 l.pack()
 inputtext.pack()
