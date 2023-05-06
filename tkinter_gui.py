fef8cffe9b7893d99430912890d2757096212ab4
Adokshaj Bhandarkar <badokshaj57@gmail.com>
2023-05-06 20:33:12 +0530

    folder cleaned

--- tkinter_gui.py
+++ tkinter_gui.py
@@ -80,7 +80,7 @@ def generate():
                 username="4nm19is120",
                 password="27102022"
                 )
-                stdin, stdout, stderr = ssh.exec_command(f"docker exec pratheek sh -c 'cd home/4nm19is120/text_to_image/text_to_image/Text-to-Image-Using-GAN-master/ && python3 connection.py --data_set=flowers --t_dim=100 --image_size=128 --data_set=flowers --z_dim=100 --n_classes=24 --caption_vector_length=4800 --batch_size=128  --checkpoints_dir=Data/training/TAC_128/checkpoints --images_per_caption=30 --data_dir=Data --text={entered_text} && cd Data && zip -r download.zip images_generated_from_text/* && ls'")
+                stdin, stdout, stderr = ssh.exec_command(f"docker exec pratheek sh -c 'cd home/4nm19is120/text_to_image/text_to_image/Text-to-Image-Using-GAN-master/ && python3 connection.py --data_set=flowers --t_dim=100 --image_size=128 --data_set=flowers --z_dim=100 --n_classes=1 --caption_vector_length=4800 --batch_size=128  --checkpoints_dir=Data/training/TAC_TEST400AUG/checkpoints --images_per_caption=30 --data_dir=Data --text={entered_text} && cd Data && zip -r download.zip images_generated_from_text/* && ls'")
                 print(stdout.read().decode())
                 sftp = ssh.open_sftp()
                 sftp.get('/home/4nm19is120/text_to_image/Text-to-Image-Using-GAN-master/Data/download.zip', 'download.zip')
